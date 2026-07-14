70b22de94a660802a1edefbeea7e0b984f9d520f
Hanikinguyen <95458424+Hanikinguyen@users.noreply.github.com>
2026-07-14 13:32:32 +0700

    Ex03

--- Ex03/Ex03.sql
+++ Ex03/Ex03.sql
@@ -1,1 +1,25 @@
+-- [Bài tập] Quản lý đơn hàng - Sản phẩm
+USE session03;
+CREATE TABLE orders (
+    order_id VARCHAR(10) PRIMARY KEY,
+    order_date DATE NOT NULL,
+    status VARCHAR(20) NOT NULL
+);
+CREATE TABLE products (
+    product_id VARCHAR(10) PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL
+);
+CREATE TABLE order_items (
+    order_id VARCHAR(10) NOT NULL,
+    product_id VARCHAR(10) NOT NULL,
+    quantity INT NOT NULL,
 
+    PRIMARY KEY (order_id, product_id),
+
+    FOREIGN KEY (order_id)
+        REFERENCES orders(order_id),
+
+    FOREIGN KEY (product_id)
+        REFERENCES products(product_id)
+);
