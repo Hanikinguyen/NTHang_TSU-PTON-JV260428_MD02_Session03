e1c1ea01c0f0e3c515d12702fd1eae3cffbf417a
Hanikinguyen <95458424+Hanikinguyen@users.noreply.github.com>
2026-07-14 13:41:20 +0700

    Ex04

--- Ex04/Ex04.sql
+++ Ex04/Ex04.sql
@@ -1,1 +1,13 @@
-
+-- [Bài tập] Quản lý tài khoản người dùng
+USE session03;
+CREATE TABLE users (
+    user_id VARCHAR(10) PRIMARY KEY,
+    username VARCHAR(50) UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    status VARCHAR(20) DEFAULT 'ACTIVE'
+);
+ALTER TABLE users
+ADD CONSTRAINT chk_status
+CHECK (status IN ('ACTIVE', 'INACTIVE'));
+DROP TABLE users;
