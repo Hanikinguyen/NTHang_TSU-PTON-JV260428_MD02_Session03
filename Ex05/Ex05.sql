9b6563e15343030c6f726c3de25275c1c6e2972b
Hanikinguyen <95458424+Hanikinguyen@users.noreply.github.com>
2026-07-14 15:14:59 +0700

    ex05

--- Ex05/Ex05.sql
+++ Ex05/Ex05.sql
@@ -1,1 +1,38 @@
+-- [Bài tập] Quản lý khóa học trực tuyến
+USE session03;
+CREATE TABLE teachers (
+    teacher_id INT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(100) NOT NULL,
+    description VARCHAR(255),
+    price DECIMAL(10,2) NOT NULL,
+    teacher_id INT NOT NULL,
 
+    FOREIGN KEY (teacher_id)
+        REFERENCES teachers(teacher_id)
+);
+CREATE TABLE students5 (
+    student_id INT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
+CREATE TABLE enrollments (
+    student_id INT,
+    course_id INT,
+
+    PRIMARY KEY (student_id, course_id),
+
+    FOREIGN KEY (student_id)
+        REFERENCES students5(student_id),
+
+    FOREIGN KEY (course_id)
+        REFERENCES courses(course_id)
+);
+ALTER TABLE courses
+ADD CONSTRAINT chk_price
+CHECK (price > 0);
+DROP TABLE enrollments;
