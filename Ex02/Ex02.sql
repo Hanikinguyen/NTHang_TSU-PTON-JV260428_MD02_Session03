e2273fcdafa8db14a705d4b1496fb99d637a222d
Hanikinguyen <95458424+Hanikinguyen@users.noreply.github.com>
2026-07-14 13:26:07 +0700

    Ex02

--- Ex02/Ex02.sql
+++ Ex02/Ex02.sql
@@ -1,1 +1,32 @@
+-- Bài tập: Quản lý thư viện
+USE session03;
+CREATE TABLE books (
+    book_id VARCHAR(10) PRIMARY KEY,
+    book_name VARCHAR(100) NOT NULL,
+    author VARCHAR(100) NOT NULL
+);
+CREATE TABLE readers (
+    reader_id VARCHAR(10) PRIMARY KEY,
+    reader_name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20)
+);
+CREATE TABLE borrowings (
+    reader_id VARCHAR(10),
+    book_id VARCHAR(10),
+    borrow_date DATE,
+    return_date DATE,
 
+    PRIMARY KEY (reader_id, book_id),
+
+    FOREIGN KEY (reader_id)
+        REFERENCES readers(reader_id),
+
+    FOREIGN KEY (book_id)
+        REFERENCES books(book_id)
+);
+ALTER TABLE borrowings
+MODIFY borrow_date DATE NOT NULL;
+SHOW TABLES;
+DESC books;
+DESC readers;
+DESC borrowings;
